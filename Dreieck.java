cbe29c7662dfb829195d378523c5eaefafc7cb06
schlaumischlumpf <schlaumischlumpf0805@gmail.com>
2024-12-06 23:47:19 +0100

    Added Java document

---
+++ Dreieck.java
@@ -0,0 +1,18 @@
+import javakara.JavaKaraProgram;
+        
+/* BEFEHLE:  kara.
+ *   move()  turnRight()  turnLeft()
+ *   putLeaf()  removeLeaf()
+ *
+ * SENSOREN: kara.
+ *   treeFront()  treeLeft()  treeRight()
+ *   mushroomFront()  onLeaf()
+ */
+
+public class Dreieck extends JavaKaraProgram {
+	
+	// Hier kommen die Methoden hin
+	public void myProgram(){
+		// Hauptprogramm
+	}
+}
